7d253b15ad92bea53ccecce7c1ebfc810396a3af
Jean-Francois Zinque <16199817+jeffzi@users.noreply.github.com>
2024-06-22 10:52:01 +0200

    feat: configure options by direct assignments

--- src/icecream.lua
+++ src/icecream.lua
@@ -46,131 +46,141 @@ local traceback = has_stack_trace_plus and stp.stacktrace or debug.traceback
 stderr:setvbuf("no")
 
 -------------------------------------------------------------------------------
+-- Config
+-------------------------------------------------------------------------------
+-- region Config
+
+local config = {
+   max_width = 80,
+   indent = "  ",
+   color = true,
+   include_context = true,
+   prefix = "ic|",
+   traceback = traceback,
+}
+
+-- endregion
+
+-------------------------------------------------------------------------------
 -- Formatting
 -------------------------------------------------------------------------------
 -- region Formatting
 
-IceCream.max_width = 80
-IceCream.indent = "  "
-IceCream.color = true
-
-local function should_wrap(s)
-   return #gsub(s, "\27%[%d+m", "") > IceCream.max_width
-end
-
-local colorizer, format_key, format_header
 do
-   local has_inspect, inspect = pcall(require, "inspect")
-   if not has_inspect then
-      inspect = function(value)
-         return value
-      end
-   end
-
    local nocolor = os.getenv("NO_COLOR")
    if nocolor and nocolor ~= "" then
       IceCream.color = false
    end
 
    local has_ansicolors, ansicolors = pcall(require, "ansicolors")
-   local colorize
-   if has_ansicolors then
-      function colorize(s, color)
-         return ansicolors("%{" .. color .. "}" .. s .. "%{reset}")
+
+   local function colorizer(color)
+      if has_ansicolors and config.color then
+         return function(s)
+            return ansicolors("%{" .. color .. "}" .. s .. "%{reset}")
+         end
       end
-   else
-      function colorize(s, _)
+      return function(s)
          return s
       end
    end
 
-   function colorizer(color)
-      return function(s)
-         return colorize(s, color)
+   function IceCream:_configure_color()
+      IceCream.format_boolean = colorizer("yellow")
+      IceCream.format_bracket = colorizer("bright white")
+      IceCream.format_header = colorizer("underline white")
+      IceCream.format_key = colorizer("blue")
+      IceCream.format_misc = colorizer("cyan")
+      IceCream.format_number = colorizer("magenta")
+      IceCream.format_string = colorizer("green")
+   end
+end
+
+local inspect
+do
+   local has_inspect
+   has_inspect, inspect = pcall(require, "inspect")
+   if not has_inspect then
+      inspect = function(value)
+         return value
       end
    end
+end
 
-   format_key = colorizer("blue")
-   format_header = colorizer("underline white")
+local INSPECT_KEY = inspect.KEY
+local function tag_key(item, path)
+   if type(item) ~= "number" and path[#path] == INSPECT_KEY and not match(item, "^__") then
+      return "@" .. item .. "@"
+   end
+   return item
+end
 
-   local format_bracketed = colorizer("cyan")
-   local format_number = colorizer("magenta")
-   local format_boolean = colorizer("yellow")
-   local format_misc = colorizer("cyan")
-   local format_string = colorizer("green")
-   local format_bracket = colorizer("bright white")
+local function should_wrap(s)
+   return #gsub(s, "\27%[%d+m", "") > config.max_width
+end
 
-   local INSPECT_KEY = inspect.KEY
-   local function tag_key(item, path)
-      if type(item) ~= "number" and path[#path] == INSPECT_KEY and not match(item, "^__") then
-         return "@" .. item .. "@"
-      end
-      return item
+local function wrap_table(s, options)
+   local indent = config.indent
+   options.indent = indent
+   options.newline = "\n" .. indent
+   ---@diagnostic disable-next-line: redundant-parameter
+   s = inspect(s, options)
+   -- indent opening bracket
+   s = gsub(s, "{", "{\n" .. indent .. " ", 1)
+   -- remove empty lines
+   s = gsub(s, "\n%s*\n", "\n")
+   return s
+end
+
+function IceCream:_prettify(s)
+   if not self.color then
+      return inspect(s)
    end
 
-   local function wrap_table(s, options)
-      local indent = IceCream.indent
-      options.indent = indent
-      options.newline = "\n" .. indent
-      ---@diagnostic disable-next-line: redundant-parameter
-      s = inspect(s, options)
-      -- indent opening bracket
-      s = gsub(s, "{", "{\n" .. indent .. " ", 1)
-      -- remove empty lines
-      s = gsub(s, "\n%s*\n", "\n")
-      return s
+   local type_ = type(s)
+   if type_ == "string" then
+      return self.format_string('"' .. s .. '"')
+   elseif type_ == "number" then
+      return self.format_number(s)
+   elseif type_ == "boolean" then
+      return self.format_boolean(tostring(s))
+   elseif type_ ~= "table" then
+      return self.format_misc(inspect(s))
    end
 
-   function IceCream:_prettify(s)
-      if not self.color then
-         return inspect(s)
-      end
-
-      local type_ = type(s)
-      if type_ == "string" then
-         return format_string('"' .. s .. '"')
-      elseif type_ == "number" then
-         return format_number(s)
-      elseif type_ == "boolean" then
-         return format_boolean(tostring(s))
-      elseif type_ ~= "table" then
-         return format_misc(inspect(s))
-      end
-
-      -- Formatting a table
-      local original = s
-      local options = { newline = " ", indent = "" }
-      options.process = tag_key
-      ---@diagnostic disable-next-line: redundant-parameter
-      s = inspect(original, options)
-      if #s > IceCream.max_width then
-         s = wrap_table(original, options)
-      end
-      s = gsub(s, '%["@(.-)@"%]', format_key)
-      s = gsub(s, "(%[%d*%])(%s=)", function(index, post)
-         return format_key(index) .. post
-      end)
-      s = gsub(s, '%b""', format_string)
-      s = gsub(s, "%b''", format_string)
-      s = gsub(s, "%b<>", format_bracketed)
-      s = gsub(s, "(-?%d*%.?%d+)(%s*[,%}\n])", function(num, post)
-         return format_number(num) .. post
-      end)
-      s = gsub(s, "inf,", format_number)
-      s = gsub(s, "(=%s*)(true)", function(pre, bool)
-         return pre .. format_boolean(bool)
-      end)
-      s = gsub(s, "(=%s*)(false)", function(pre, bool)
-         return pre .. format_boolean(bool)
-      end)
-      s = gsub(s, "(__[a-z]+)(%s*=)", function(fn, pre)
-         -- format metamethod
-         return format_misc(fn) .. pre
-      end)
-      s = gsub(s, "([{}])", format_bracket)
-
-      return s
+   -- Formatting a table
+   local original = s
+   local options = { newline = " ", indent = "" }
+   options.process = tag_key
+   ---@diagnostic disable-next-line: redundant-parameter
+   s = inspect(original, options)
+   if #s > config.max_width then
+      s = wrap_table(original, options)
    end
+   s = gsub(s, '%["@(.-)@"%]', self.format_key)
+   s = gsub(s, "(%[%d*%])(%s=)", function(index, post)
+      return self.format_key(index) .. post
+   end)
+   s = gsub(s, '%b""', self.format_string)
+   s = gsub(s, "%b''", self.format_string)
+   s = gsub(s, "%b<>", self.format_misc)
+   s = gsub(s, "(-?%d*%.?%d+)(%s*[,%}\n])", function(num, post)
+      return self.format_number(num) .. post
+   end)
+   s = gsub(s, "inf,", self.format_number)
+   s = gsub(s, "(=%s*)(true)", function(pre, bool)
+      return pre .. self.format_boolean(bool)
+   end)
+   s = gsub(s, "(=%s*)(false)", function(pre, bool)
+      return pre .. self.format_boolean(bool)
+   end)
+   s = gsub(s, "(__[a-z]+)(%s*=)", function(fn, pre)
+      -- format metamethod
+      return self.format_misc(fn) .. pre
+   end)
+   s = gsub(s, "([{}])", self.format_bracket)
+
+   return s
 end
 
 -- endregion
@@ -253,31 +263,12 @@ end
 -------------------------------------------------------------------------------
 -- region public
 
-IceCream.include_context = true
-IceCream.prefix = "ic|"
-
 IceCream.output_function = function(s)
    stderr:write(s)
 end
 
-function IceCream:configure(include_context, prefix, output_function)
-   if include_context ~= nil then
-      self.include_context = include_context
-   end
-   if prefix ~= nil then
-      if type(prefix) == "function" then
-         self.prefix = prefix()
-      else
-         self.prefix = prefix
-      end
-   end
-   if output_function ~= nil then
-      self.output_function = output_function
-   end
-end
-
 function IceCream:_format(...)
-   local include_context = self.include_context
+   local include_context = config.include_context
    if include_context == nil then
       include_context = true
    end
@@ -285,23 +276,26 @@ function IceCream:_format(...)
    local info = getinfo(3, "Sln")
 
    local location = info.short_src .. ":" .. info.currentline
-   local header = self.prefix
+   local header = config.prefix
 
    if include_context then
       header = header .. " " .. location
       local fn_name = info.name
       if fn_name then
          header = header .. " <" .. fn_name .. ">"
+      else
+         header = header .. ":"
       end
    end
-   header = format_header(header)
+   header = self.format_header(header)
 
    local arg_count = select("#", ...)
    if arg_count == 0 then
-      return header .. " " .. (traceback("", 3) or "")
+      return header .. " " .. (config.traceback("", 3) or "")
    end
 
    local keys, key_count = parse_aliases(info)
+   print(keys, key_count, select("#", ...))
    if not "keys" or key_count ~= select("#", ...) then
       error("Failed to parse arguments from source @" .. location)
    end
@@ -314,7 +308,7 @@ function IceCream:_format(...)
       if not key or key == tostring(value) then
          key = ""
       else
-         key = format_key(key) .. " = "
+         key = self.format_key(key) .. " = "
       end
 
       pretty_args[i] = key .. self:_prettify(value)
@@ -322,7 +316,7 @@ function IceCream:_format(...)
 
    local output = tconcat(pretty_args, ", ")
    if should_wrap(output) then
-      local sep = "\n" .. self.indent
+      local sep = "\n" .. config.indent
       output = sep .. tconcat(pretty_args, sep)
    end
 
@@ -364,6 +358,30 @@ function IceCream:disable()
    self.enabled = false
 end
 
+local mt = {
+   __index = function(_, k)
+      return config[k]
+   end,
+   __newindex = function(_, k, v)
+      if config[k] == nil then
+         error(k .. " is not a valid config option.")
+      end
+
+      if v == nil then
+         error(k .. " option cannot be set to nil.")
+      end
+
+      config[k] = v
+
+      if k == "color" then
+         IceCream:_configure_color()
+      end
+   end,
+   __call = IceCream.ic,
+}
+
+IceCream:_configure_color()
+
 -- endregion
 
-return setmetatable(IceCream, { __call = IceCream.ic })
+return setmetatable(IceCream, mt)
